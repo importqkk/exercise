e72f32e775f536ff5f483058f07b1089bd113043
importqkk <import_qkk@naver.com>
2023-01-05 21:48:15 +0900

    Exercise01_2 선택정렬 연습 3

---
+++ Exercise/src/day07/Exercise01_2.java
@@ -0,0 +1,36 @@
+package day07;
+
+public class Exercise01_2 {
+	public static void main(String[] args) {
+		
+		// 선택정렬
+		
+		// 데이터: 3, 1, 5, 4, 7, 2, 6
+		
+		// 데이터 생성
+		int[] data = new int[] {3, 5, 1, 4, 7, 2, 6};
+		
+		
+		// 처리
+		for(int k = 0; k < data.length - 1; k++) {
+			int index = k;
+			for(int i = k + 1; i < data.length; i++) {
+				if(data[index] > data[i]) {
+					index = i;
+				}
+			}
+			int backup = data[k];
+			data[k] = data[index];
+			data[index] = backup;
+		}
+		
+		
+		
+		// 출력
+		for(int i=0; i<data.length; i++) {
+			System.out.print(data[i]);
+			System.out.print(" ");
+		}
+
+	}
+}
