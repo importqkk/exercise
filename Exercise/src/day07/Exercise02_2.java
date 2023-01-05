676a652286d2ec7edc0392025e0c855b9a62f001
importqkk <import_qkk@naver.com>
2023-01-05 22:28:22 +0900

    Exercise02_2 버블정렬 연습 3

---
+++ Exercise/src/day07/Exercise02_2.java
@@ -0,0 +1,33 @@
+package day07;
+
+public class Exercise02_2 {
+	public static void main(String[] args) {
+		
+		// 버블정렬
+		
+		// 데이터: 3, 1, 5, 4, 7, 2, 6
+		
+		// 데이터 생성
+		int[] data = new int[] {3, 5, 1, 4, 7, 2, 6};
+		
+		
+		// 처리
+		
+		for(int k = data.length - 1; k > 0; k--) {
+			for(int i = 0; i < k; i++) {
+				if(data[i] > data[i+1]) {
+					int backup = data[i];
+					data[i] = data[i+1];
+					data[i+1] = backup;
+				}
+			}
+		}
+		
+		
+		// 출력
+		for(int i = 0; i < data.length; i++) {
+			System.out.print(data[i]);
+			System.out.print(" ");
+		}
+	}
+}
