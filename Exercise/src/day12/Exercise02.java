83e8f7fea5f7b00abb7c0bd869f6c0460c1c0fa5
importqkk <import_qkk@naver.com>
2023-01-15 22:54:03 +0900

    쿵쿵따 (substring, equals, try/catch)

--- Exercise/src/day12/Exercise02.java
+++ Exercise/src/day12/Exercise02.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Exercise02 {
 	public static void main(String[] args) {
 		
-		String start = "쿵쿵따";
+		String start = "기차표";
 		System.out.println("쿵쿵따 시작!");
-		System.out.println("\"" + start + "\"" + " 쿵쿵따");
+		System.out.println(start + " 쿵쿵따");
 		
 		try(Scanner scan = new Scanner(System.in);) {	
 			
@@ -18,16 +18,16 @@ public class Exercise02 {
 				String a = answer.substring(0, 1);
 				
 				if(answer.length() == 3 && a.equals(s)) {
-					System.out.println("\"" + answer + "\"" + " 쿵쿵따");
+					System.out.println("쿵쿵따");
 					start = answer;
 				}
 				else {
 					System.out.println("GAME OVER!");
 					break;
 				}
+				
 			}
 		}
-		
 		catch(Exception e) {
 //			e.printStackTrace();
 			System.out.println("오류가 발생했습니다.");
