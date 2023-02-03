858536a64db8aa49724195a7969ad9914a96fbff
importqkk <import_qkk@naver.com>
2023-02-03 19:33:07 +0900

    ex230203 생성

---
+++ ex230203/src/test/java/com/exercise/ex230203/Ex230203ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.ex230203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex230203ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
