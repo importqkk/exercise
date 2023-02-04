5a3fe86b5ff83ddf4775ce820cd2fb5e690183a5
importqkk <import_qkk@naver.com>
2023-02-04 23:48:39 +0900

    ex230202 재생성

---
+++ ex230202/src/main/java/com/exercise/ex230202/Ex230202Application.java
@@ -0,0 +1,13 @@
+package com.exercise.ex230202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex230202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex230202Application.class, args);
+	}
+
+}
