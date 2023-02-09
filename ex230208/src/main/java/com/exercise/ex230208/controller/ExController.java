2a305368ec6a18d111701739a27d82f4fd4ad009
importqkk <import_qkk@naver.com>
2023-02-09 21:31:39 +0900

    기초문법

--- ex230208/src/main/java/com/exercise/ex230208/controller/ExController.java
+++ ex230208/src/main/java/com/exercise/ex230208/controller/ExController.java
@@ -10,19 +10,4 @@ public class ExController {
 		return "/WEB-INF/views/basic.jsp";
 	}
 	
-	@RequestMapping("/img")
-	public String img() {
-		return "/WEB-INF/views/img.jsp";
-	}
-	
-	@RequestMapping("/aTag")
-	public String aTag() {
-		return "/WEB-INF/views/aTag.jsp";
-	}
-	
-	@RequestMapping("/form")
-	public String form() {
-		return "/WEB-INF/views/form.jsp";
-	}
-	
 }
