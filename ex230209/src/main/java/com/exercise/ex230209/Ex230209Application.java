ff19788516c5e0f45c11f31f189c1609e4e277b4
importqkk <import_qkk@naver.com>
2023-02-09 20:24:05 +0900

    ex230209 생성

---
+++ ex230209/src/main/java/com/exercise/ex230209/Ex230209Application.java
@@ -0,0 +1,13 @@
+package com.exercise.ex230209;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex230209Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex230209Application.class, args);
+	}
+
+}
---
+++ ex230209/src/test/java/com/exercise/ex230209/Ex230209ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.ex230209;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex230209ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
