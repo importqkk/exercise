6954393a5e5f75f54ab98fa4c874204d1b0d4141
importqkk <import_qkk@naver.com>
2023-02-10 21:44:50 +0900

    ex230210 생성

---
+++ ex230210/src/main/java/com/exercise/ex230210/Ex230210Application.java
@@ -0,0 +1,13 @@
+package com.exercise.ex230210;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex230210Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex230210Application.class, args);
+	}
+
+}
---
+++ ex230210/src/test/java/com/exercise/ex230210/Ex230210ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.ex230210;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex230210ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
