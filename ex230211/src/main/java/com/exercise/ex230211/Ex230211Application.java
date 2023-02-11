66761f718d3d6c6af0790fbb5372c45773d147c3
importqkk <import_qkk@naver.com>
2023-02-11 21:36:35 +0900

    ex230211 생성

---
+++ ex230211/src/main/java/com/exercise/ex230211/Ex230211Application.java
@@ -0,0 +1,13 @@
+package com.exercise.ex230211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex230211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex230211Application.class, args);
+	}
+
+}
---
+++ ex230211/src/main/java/com/exercise/ex230211/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.exercise.ex230211;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Ex230211Application.class);
+	}
+
+}
