bf4124df32e90b6fc6e2135843162dec740cd2f7
importqkk <import_qkk@naver.com>
2023-02-11 23:22:19 +0900

    강의 dto

---
+++ ex230211/src/main/java/com/exercise/ex230211/dto/LecturesDto.java
@@ -0,0 +1,13 @@
+package com.exercise.ex230211.dto;
+import lombok.Data;
+
+@Data
+public class LecturesDto {
+
+	private int lecturesNo;
+	private String lecturesLecture;
+	private String lecturesLecturer;
+	private int lecturesHours;
+	private int lecturesFee;
+	
+}
