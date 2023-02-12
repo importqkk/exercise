23de9fa9d35ed37a22cfe895f30ea07c5a41aa28
importqkk <import_qkk@naver.com>
2023-02-12 21:37:31 +0900

    강의 dao(230212)

--- ex230211/src/main/java/com/exercise/exPage/dao/LecturesDao.java
+++ ex230211/src/main/java/com/exercise/exPage/dao/LecturesDao.java
@@ -1,12 +1,14 @@
-package com.exercise.ex230211.dao;
+package com.exercise.exPage.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import com.exercise.ex230211.dto.LecturesDto;
+
+import com.exercise.exPage.dto.LecturesDto;
 
 @Repository
 public class LecturesDao {
