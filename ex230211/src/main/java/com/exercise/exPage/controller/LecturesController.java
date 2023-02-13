96c41d6bfbf6d50bbb71697f29a143c35196dff3
importqkk <import_qkk@naver.com>
2023-02-13 21:52:43 +0900

    강의 컨트롤러(230213)

--- ex230211/src/main/java/com/exercise/exPage/controller/LecturesController.java
+++ ex230211/src/main/java/com/exercise/exPage/controller/LecturesController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.exercise.exPage.dao.LecturesDao;
 import com.exercise.exPage.dto.LecturesDto;
 
