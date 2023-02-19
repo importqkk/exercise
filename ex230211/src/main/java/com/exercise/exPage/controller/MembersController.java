4c3bb29e8c29339c40bdb2edd39e54e567b22475
importqkk <import_qkk@naver.com>
2023-02-19 23:41:04 +0900

    회원 컨트롤러 (230219)

--- ex230211/src/main/java/com/exercise/exPage/controller/MembersController.java
+++ ex230211/src/main/java/com/exercise/exPage/controller/MembersController.java
@@ -1,5 +1,6 @@
 package com.exercise.exPage.controller;
 import java.io.IOException;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.exercise.exPage.dao.MembersDao;
 import com.exercise.exPage.dto.MembersDto;
 import com.exercise.exPage.service.MembersService;
@@ -34,6 +36,43 @@ public class MembersController {
 	@GetMapping("/joinComplete") 
 	public String joinComplete() {
 		return "/WEB-INF/views/members/joinComplete.jsp";
+	}
+	
+	// 로그인
+	@GetMapping("/login")
+	public String login() {
+		return "/WEB-INF/views/members/login.jsp";
+	}
+	@PostMapping("/login")
+	public String login(@ModelAttribute MembersDto inDto,
+			HttpSession session, RedirectAttributes attr) {
+		// 로그인 검사
+		MembersDto membersDto = membersDao.selectOne(inDto.getMemberID());
+		// 없는 아이디라면
+		if(membersDto == null) {
+			attr.addAttribute("mode", "error");
+			return "redirect:login";
+		}
+		// 비밀번호가 일치하지 않는다면
+		if(!inDto.getMemberPW().equals(membersDto.getMemberPW())) {
+			attr.addAttribute("mode", "error");
+			return "redirect:login";
+		}
+		// 세션에 아이디, 레벨 정보 추가
+		session.setAttribute("memberID", membersDto.getMemberID());
+		session.setAttribute("memberLevel", membersDto.getMemberLevel());
+		// 최종로그인시각 갱신
+		membersDao.updateLoginDate(membersDto.getMemberID());
+		// 로그인 성공 -> 메인으로 리다이렉트
+		return "redirect:/";		
+	}
+	
+	// 로그아웃
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("memberID");
+		session.removeAttribute("memberLevel");
+		return "redirect:/";
 	}
 	
 }
