2465434677f3e631191652f46cb2f3393c538004
importqkk <import_qkk@naver.com>
2023-02-20 23:46:07 +0900

    회원정보+이미지 연결 dao(230220)

--- ex230211/src/main/java/com/exercise/exPage/dao/MembersImgDao.java
+++ ex230211/src/main/java/com/exercise/exPage/dao/MembersImgDao.java
@@ -1,6 +1,7 @@
 package com.exercise.exPage.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -28,6 +29,14 @@ public class MembersImgDao {
 		String sql = "insert into members_img values(?, ?)";
 		Object[] param = {membersImgDto.getMemberID(), membersImgDto.getAttachmentNo()};
 		jdbcTemplate.update(sql, param);		
+	}
+	
+	// 프로필 이미지 조회
+	public MembersImgDto selectOne(String memberID) {
+		String sql = "select * from members_img where member_id = ?";
+		Object[] param = {memberID};
+		List<MembersImgDto> list = jdbcTemplate.query(sql, mapper, param);
+		return list.isEmpty() ? null : list.get(0);
 	}
 	
 }
