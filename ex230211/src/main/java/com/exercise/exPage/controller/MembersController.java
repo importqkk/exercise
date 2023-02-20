fff40d6ceceb9958a2c528a53115a57871950a8a
importqkk <import_qkk@naver.com>
2023-02-20 23:45:48 +0900

    회원 컨트롤러(230220)

--- ex230211/src/main/java/com/exercise/exPage/controller/MembersController.java
+++ ex230211/src/main/java/com/exercise/exPage/controller/MembersController.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.exercise.exPage.dao.MembersDao;
+import com.exercise.exPage.dao.MembersImgDao;
 import com.exercise.exPage.dto.MembersDto;
 import com.exercise.exPage.service.MembersService;
 
@@ -21,6 +23,8 @@ public class MembersController {
 	private MembersDao membersDao;
 	@Autowired
 	private MembersService membersService;
+	@Autowired
+	private MembersImgDao membersImgDao;
 	
 	// 회원가입
 	@GetMapping("/join")
@@ -73,6 +77,16 @@ public class MembersController {
 		session.removeAttribute("memberID");
 		session.removeAttribute("memberLevel");
 		return "redirect:/";
+	}
+	
+	// 마이페이지
+	@GetMapping("/mypage")
+	public String mypage(HttpSession session, Model model) {
+		String membrerID = (String)session.getAttribute("memberID");
+		MembersDto membersDto =	membersDao.selectOne(membrerID);
+		model.addAttribute("membersDto", membersDto);
+		model.addAttribute("profile", membersImgDao.selectOne(membrerID));
+		return "/WEB-INF/views/members/mypage.jsp";
 	}
 	
 }
