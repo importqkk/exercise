2632f3b59d57e1c563f629ad6e083cc96d870ea6
importqkk <import_qkk@naver.com>
2023-02-21 22:17:29 +0900

    강의정보+이미지 연결 dao(230221)

--- ex230211/src/main/java/com/exercise/exPage/dao/LectureImgDao.java
+++ ex230211/src/main/java/com/exercise/exPage/dao/LectureImgDao.java
@@ -1,6 +1,8 @@
 package com.exercise.exPage.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -28,6 +30,14 @@ public class LectureImgDao {
 		String sql = "insert into lecture_img(lecture_no, attachment_no) values(?, ?)";
 		Object[] param = {lectureImgDto.getLectureNo(), lectureImgDto.getAttachmentNo()};
 		jdbcTemplate.update(sql, param);
+	}
+	
+	// 이미지 조회
+	public LectureImgDto selectOne(int lectureNo) {
+		String sql = "select * from lecture_img where lecture_no = ?";
+		Object[] param = {lectureNo};
+		List<LectureImgDto> list = jdbcTemplate.query(sql, mapper, param);
+		return list.isEmpty() ? null : list.get(0);
 	}
 	
 }
