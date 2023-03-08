5f2785cb60e04bcc336eea8ef74023f07a5057cf
importqkk <import_qkk@naver.com>
2023-03-08 22:44:00 +0900

    관리자 컨트롤러 (230308)

--- ex230211/src/main/java/com/exercise/exPage/controller/AdminController.java
+++ ex230211/src/main/java/com/exercise/exPage/controller/AdminController.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.exercise.exPage.dao.MembersDao;
+import com.exercise.exPage.dao.MembersImgDao;
 import com.exercise.exPage.dto.MembersDto;
 import com.exercise.exPage.vo.PageVO;
 
@@ -16,12 +20,16 @@ public class AdminController {
 
 	@Autowired
 	private MembersDao membersDao;
+	@Autowired
+	private MembersImgDao membersImgDao;
 	
+	// 관리 메인
 	@GetMapping
 	public String main() {
 		return "/WEB-INF/views/admin/main.jsp";
 	}
 	
+	// 회원 목록
 	@GetMapping("/members/list")
 	public String membersList(Model model, @ModelAttribute("vo") PageVO vo) {
 		int totalMembers = membersDao.listCount(vo);
@@ -29,6 +37,43 @@ public class AdminController {
 		List<MembersDto> list = membersDao.list(vo);
 		model.addAttribute("list", list);
 		return "/WEB-INF/views/admin/members/list.jsp";
+	}
+	
+	// 회원 강제 탈퇴
+	@GetMapping("/members/leave")
+	public String membersLeave(@RequestParam String memberID,
+			@RequestParam int page, RedirectAttributes attr) {
+		MembersDto membersDto = membersDao.selectOne(memberID);
+		membersDao.leave(memberID);
+		membersDao.waiting(membersDto);
+		attr.addAttribute("page", page);
+		return "redirect:list";
+		
+	}
+	
+	// 회원 상세정보
+	@GetMapping("/members/detail")
+	public String membersDetail(Model model, @RequestParam String memberID) {
+		MembersDto membersDto = membersDao.selectOne(memberID);
+		model.addAttribute("membersDto", membersDto);
+		model.addAttribute("profile", membersImgDao.selectOne(membersDto.getMemberID()));
+		return "/WEB-INF/views/admin/members/detail.jsp";
+	}
+	
+	// 회원 정보 수정
+	@GetMapping("/members/changeInfo")
+	public String membersChangeInfo(Model model,
+			@RequestParam String memberID) {
+		MembersDto membersDto = membersDao.selectOne(memberID);
+		model.addAttribute("membersDto", membersDto);
+		return "/WEB-INF/views/admin/members/changeInfo.jsp";
+	}
+	@PostMapping("/members/changeInfo")
+	public String membersChangeInfo(RedirectAttributes attr,
+			@ModelAttribute MembersDto membersDto) {
+		membersDao.adminChangeInfo(membersDto);
+		attr.addAttribute("memberID", membersDto.getMemberID());
+		return "redirect:detail";
 	}
 	
 }
