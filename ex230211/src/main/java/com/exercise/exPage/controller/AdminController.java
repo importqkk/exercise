68936ff7fbba212fa3fec6c21b5038ec30e62065
importqkk <import_qkk@naver.com>
2023-03-09 22:46:19 +0900

    관리자 컨트롤러 (230309)

--- ex230211/src/main/java/com/exercise/exPage/controller/AdminController.java
+++ ex230211/src/main/java/com/exercise/exPage/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.exercise.exPage.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import com.exercise.exPage.component.RandomStringComponent;
 import com.exercise.exPage.dao.MembersDao;
 import com.exercise.exPage.dao.MembersImgDao;
 import com.exercise.exPage.dto.MembersDto;
@@ -22,6 +25,10 @@ public class AdminController {
 	private MembersDao membersDao;
 	@Autowired
 	private MembersImgDao membersImgDao;
+	@Autowired
+	private RandomStringComponent ranStringComponent;
+	@Autowired
+	private JavaMailSender sender;
 	
 	// 관리 메인
 	@GetMapping
@@ -74,6 +81,30 @@ public class AdminController {
 		membersDao.adminChangeInfo(membersDto);
 		attr.addAttribute("memberID", membersDto.getMemberID());
 		return "redirect:detail";
+	}
+	
+	// 회원 임시비밀번호 발급
+	@GetMapping("/members/tempPW")
+	public String membersTempPW(@ModelAttribute MembersDto membersDto) {
+		// 회원정보 조회
+		String memberID = membersDto.getMemberID();
+		membersDto = membersDao.selectOne(memberID);
+		// 임시비밀번호 생성
+		String tempPW = ranStringComponent.generateString();
+		// 생성된 임시비밀번호로 비번 변경
+		membersDao.changePW(memberID, tempPW);
+		// 메일 작성
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(membersDto.getMemberEmail());
+		message.setSubject("[개발하는 쿼카] 임시 비밀번호 발급");
+		message.setText("회원님의 임시 비밀번호는 "+tempPW+" 입니다. 로그인 후 비밀번호를 반드시 변경해주시길 바랍니다.");
+		// 메일 발송
+		sender.send(message);
+		return "redirect:tempPWComplete";
+	}
+	@GetMapping("/members/tempPWComplete")
+	public String membersTempPWComplete() {
+		return "/WEB-INF/views/admin/members/tempPW.jsp";
 	}
 	
 }
