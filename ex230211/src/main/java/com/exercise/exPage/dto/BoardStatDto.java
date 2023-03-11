313f6debf095e791bf8660693e63cf3d4d6ac1b9
importqkk <import_qkk@naver.com>
2023-03-11 23:52:56 +0900

    게시판 통계 dto (230311)

---
+++ ex230211/src/main/java/com/exercise/exPage/dto/BoardStatDto.java
@@ -0,0 +1,14 @@
+package com.exercise.exPage.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor
+@AllArgsConstructor @Builder
+public class BoardStatDto {
+
+	private String boardCategory;
+	private int cnt;
+	
+}
