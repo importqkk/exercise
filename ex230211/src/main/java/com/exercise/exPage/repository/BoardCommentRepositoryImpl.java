9ae4ecd3d5651fcfa6a6c15d4e8b3a30483b20c1
importqkk <import_qkk@naver.com>
2023-04-14 22:26:51 +0900

    게시판 댓글 레파지토리 imple (230414)

--- ex230211/src/main/java/com/exercise/exPage/repository/BoardCommentRepositoryImpl.java
+++ ex230211/src/main/java/com/exercise/exPage/repository/BoardCommentRepositoryImpl.java
@@ -2,8 +2,11 @@ package com.exercise.exPage.repository;
 import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.exercise.exPage.dto.BoardCommentDto;
 
+@Repository
 public class BoardCommentRepositoryImpl implements BoardCommentRepository {
 
 	@Autowired
@@ -15,8 +18,24 @@ public class BoardCommentRepositoryImpl implements BoardCommentRepository {
 	}
 
 	@Override
-	public List<BoardCommentDto> selectList() {
+	public List<BoardCommentDto> selectList(int boardNo) {
 		return sql.selectList("comment.list");
+	}
+
+	@Override
+	public BoardCommentDto selectOne(int commentNo) {
+		return sql.selectOne("comment.selectOne", commentNo);
+	}
+
+	@Override
+	public boolean update(BoardCommentDto boardCommentDto) {
+		return sql.update("comment.edit", boardCommentDto) > 0;
+		
+	}
+
+	@Override
+	public boolean delete(int commentNo) {
+		return sql.delete("comment.delete", commentNo) > 0;		
 	}
 	
 }
