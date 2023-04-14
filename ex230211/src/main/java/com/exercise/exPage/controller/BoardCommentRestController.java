3a7750bfbf3640d09ca3dbc2ca6f3d7f9a21c4e5
importqkk <import_qkk@naver.com>
2023-04-14 22:27:04 +0900

    게시판 댓글 rest 컨트롤러 (230414)

---
+++ ex230211/src/main/java/com/exercise/exPage/controller/BoardCommentRestController.java
@@ -0,0 +1,51 @@
+package com.exercise.exPage.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.exercise.exPage.dao.BoardDao;
+import com.exercise.exPage.dto.BoardCommentDto;
+import com.exercise.exPage.repository.BoardCommentRepository;
+
+@RestController
+@RequestMapping("/rest/comment")
+public class BoardCommentRestController {
+
+	@Autowired
+	private BoardCommentRepository commentRepo;
+	@Autowired
+	private BoardDao boardDao;
+	
+	// 목록
+	@GetMapping("/{boardNo}")
+	public List<BoardCommentDto> list(@PathVariable int boardNo) {
+		return commentRepo.selectList(boardNo);
+	}
+	
+	// 등록
+	@PostMapping("/")
+	public void insert(@ModelAttribute BoardCommentDto boardCommentDto) {
+		commentRepo.insert(boardCommentDto);
+	}
+	
+	// 수정
+	@PatchMapping("/")
+	public void edit(@ModelAttribute BoardCommentDto boardCommentDto) {
+		commentRepo.update(boardCommentDto);
+	}
+	
+	// 삭제
+	@DeleteMapping("/{commentNo}")
+	public void delete(@PathVariable int commentNo) {
+		BoardCommentDto boardCommentDto = commentRepo.selectOne(commentNo);
+		commentRepo.delete(commentNo);
+		boardDao.updateCommentCount(boardCommentDto.getBoardNo());
+	}
+	
+}
