eb1f88ee5f667f8791dd38fcb86795e70e7c2498
importqkk <import_qkk@naver.com>
2023-04-20 23:18:51 +0900

    라이트픽 js (230420)

--- ex230211/src/main/resources/static/js/date-picker.js
+++ ex230211/src/main/resources/static/js/date-picker.js
@@ -6,3 +6,20 @@ window.addEventListener("load", function(){
     }
     var picker = new Lightpick(options);
 });
+window.addEventListener("load", function(){
+    var options = {
+        field: document.querySelector('.date-picker2'),
+        format: "YYYY-MM-DD",   // 날짜 형식
+        maxDate: moment()
+    }
+    var picker = new Lightpick(options);
+});
+
+window.addEventListener("load", function(){
+    var options = {
+        field: document.querySelector('.date-picker3'),
+        format: "YYYY-MM-DD",   // 날짜 형식
+        maxDate: moment()
+    }
+    var picker = new Lightpick(options);
+});
