6d12a879c59857025173909808d7f11156217b1e
importqkk <import_qkk@naver.com>
2023-04-27 23:35:57 +0900

    강의 rest 컨트롤러 (230427)

--- ex230211/src/main/java/com/exercise/exPage/controller/LecturesRestController.java
+++ ex230211/src/main/java/com/exercise/exPage/controller/LecturesRestController.java
@@ -1,10 +1,16 @@
 package com.exercise.exPage.controller;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.exercise.exPage.dto.LecturesDto;
 import com.exercise.exPage.repository.LectureRepository;
@@ -20,6 +26,11 @@ public class LecturesRestController {
 	@PostMapping("/")
 	public void insert(@ParameterObject @RequestBody LecturesDto lecturesDto) {
 		lectureRepository.insert(lecturesDto);
+	}
+	
+	@GetMapping("/")
+	public List<LecturesDto> selectList(Model model) {
+		return lectureRepository.selectList();
 	}
 	
 }
