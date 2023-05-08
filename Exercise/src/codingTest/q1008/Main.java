989344b753f3c476caae9e35a7ab8e39e9fd04be
importqkk <import_qkk@naver.com>
2023-05-08 21:49:41 +0900

    230508

---
+++ Exercise/src/codingTest/q1008/Main.java
@@ -0,0 +1,16 @@
+package codingTest.q1008;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a / b);
+		
+	}
+}
