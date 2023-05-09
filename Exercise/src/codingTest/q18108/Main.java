7d2232b902fd92e3566f708c529da3e3fd817abe
importqkk <import_qkk@naver.com>
2023-05-09 22:06:37 +0900

    230509

---
+++ Exercise/src/codingTest/q18108/Main.java
@@ -0,0 +1,17 @@
+package codingTest.q18108;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		sc.close();
+		
+		int gap = 543;
+		
+		System.out.println(y - gap);
+		
+	}
+}
